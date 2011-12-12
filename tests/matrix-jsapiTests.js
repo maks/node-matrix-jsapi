9adca0686e2b44110f3d5fc8521f0383802da9af
Maksim Lin <maks@manichord.com>
2011-12-12 12:58:35 +1100

    changed API callback to return error (if any) as first arguement to match NodeJS conventions and updated tests.

--- tests/matrix-jsapiTests.js
+++ tests/matrix-jsapiTests.js
@@ -5,28 +5,22 @@
 var log = require("mkslogger").logger(module),
     vows = require('vows'),
     assert = require('assert'),
-    mapi = require('../lib/matrix-api'),
+    mapi = require('../lib/matrix-api');
     
-    API_USER = 'jsapi',
-    API_PASSWORD = 'testjsapi',
-    API_KEY = '1064090338',
-    API_URL = 'http://matrixdemo.squiz.net/test.js';
+    mapi.setApiKey('1064090338');
+    mapi.setApiUrl('http://matrixdemo.squiz.net/test.js');
+    
+    mapi.setUsername('jsapi');
+    mapi.setPassword('testjsapi');
 
-
-exports.suite1 = vows.describe('Create Asset');
+exports.suite1 = vows.describe('Creating Assets');
 
 exports.suite1.addBatch({
-    'New Standard Page': {
+    'when creating a Standard Page': {
         topic: function() {
             var parentAssetID = 72,
                 nuAssetName = 'Test Asset'+Date.now(),
                 self = this;
-        
-            mapi.setApiKey(API_KEY);
-            mapi.setApiUrl(API_URL);
-            
-            mapi.setUsername(API_USER);
-            mapi.setPassword(API_PASSWORD);
             
             mapi.createAsset(
                 parentAssetID,
@@ -39,16 +33,16 @@ exports.suite1.addBatch({
                 '0', //is_exclusive
                 '0', //extra_attributes
                 '',
-                function(data) {
-                        self.callback(data.error, data);    
-                }
+                self.callback
             );
-             
         },
-        'results in ID of new asset': function(result) {
-            log.debug("result:"+result);
+        'returns a valid asset id' : function(err, result) {
+            log.debug("result:",result);
             assert.isNotNull(result);
             assert.isNumber(+result.id);
+        },
+        'return a field with the correct name': function(err, result) {
+            //assert.equal(nuName, result.name);
         }
     }
 }); 
